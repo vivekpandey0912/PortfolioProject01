1875b50c857e072ae0c212390f744b73dcd19bed
Vivek Pandey <vp1446392@gmail.com>
2023-07-20 14:16:32 +0530

    added package for import scematic in angular application

--- PortFolio/src/app/app.module.ts
+++ PortFolio/src/app/app.module.ts
@@ -16,6 +16,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
