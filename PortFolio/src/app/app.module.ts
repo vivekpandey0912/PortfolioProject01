fe3e4001d71d9f15c153244f9a319ed08804ff7e
Vivek Pandey <vp1446392@gmail.com>
2023-07-20 14:23:52 +0530

    created realtion parent child

--- PortFolio/src/app/app.module.ts
+++ PortFolio/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 
 
@@ -26,7 +27,8 @@ import { MatListModule } from '@angular/material/list';
     WorkedComponent,
     ContactComponent,
     HomeComponent,
-    HomepageComponent
+    HomepageComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
